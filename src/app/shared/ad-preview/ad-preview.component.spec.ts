9d7387d4166f75ce42c3c7a2da78bfa6b4ec7952
JT-Bruch <jt.bruch@crownpeak.com>
2017-01-19 09:00:16 -0700

    NS: Fix thing.

--- src/app/shared/ad-preview/ad-preview.component.spec.ts
+++ src/app/shared/ad-preview/ad-preview.component.spec.ts
@@ -3,9 +3,9 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AdPreviewComponent } from './ad-preview.component';
 
-describe('Component: TinyMce', () => {
-//   xit('should create an instance', () => {
-//     let component = new AdPreviewComponent();
-//     expect(component).toBeTruthy();
-//   });
-// });
+// describe('Component: TinyMce', () => {
+// //   xit('should create an instance', () => {
+// //     let component = new AdPreviewComponent();
+// //     expect(component).toBeTruthy();
+// //   });
+// // });
