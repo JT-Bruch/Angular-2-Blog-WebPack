d3b761b5f53b0bc43104c20f7be8203dbd4405c3
JT-Bruch <jt.bruch@crownpeak.com>
2017-01-23 19:26:09 -0700

    NS: Adds login modules.

---
+++ src/app/pages/auth-page/signup/signup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  styleUrls: ['./signup.component.scss'],
+  templateUrl: './signup.component.html'
+})
+export class SignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
